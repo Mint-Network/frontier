31e2c38c41caca798e1069d6d7b3f5a7d69c43c1
Ayush <>
2022-04-05 09:10:53 +0200

    Added files

---
+++ frame/dynamic-fee/src/lib.rs
@@ -0,0 +1,146 @@
+// SPDX-License-Identifier: Apache-2.0
+// This file is part of Frontier.
+//
+// Copyright (c) 2021 Parity Technologies (UK) Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Ensure we're `no_std` when compiling for Wasm.
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use codec::{Encode, Decode};
+use sp_std::{result, cmp::{min, max}};
+use sp_runtime::RuntimeDebug;
+use sp_core::U256;
+use sp_inherents::{InherentIdentifier, InherentData, ProvideInherent, IsFatalError};
+#[cfg(feature = "std")]
+use sp_inherents::ProvideInherentData;
+use frame_support::{traits::Get};
+
+use frame_system::ensure_none;
+pub use pallet::*;
+
+/// Implementation of Mixer pallet
+#[frame_support::pallet]
+pub mod pallet {
+	use super::*;
+	use frame_support::pallet_prelude::*;
+	use frame_system::pallet_prelude::*;
+
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		/// The overarching event type.
+		type Event: IsType<<Self as frame_system::Config>::Event> + From<Event<Self>>;
+		/// Bound divisor for min gas price.
+		type MinGasPriceBoundDivisor: Get<U256>;
+	}
+
+	#[pallet::storage]
+	#[pallet::getter(fn target_min_gas_price)]
+	pub type TargetMinGasPrice<T: Config> = StorageValue<_, Option<U256>, ValueQuery>;
+
+	#[pallet::storage]
+	#[pallet::getter(fn min_gas_price)]
+	pub type MinGasPrice<T: Config> = StorageValue<_, U256, ValueQuery>;
+
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		TargetMinGasPriceSet(U256)
+	}
+
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(PhantomData<T>);
+
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
+		fn on_finalize(_n: T::BlockNumber) {
+			if let Some(target) = TargetMinGasPrice::<T>::get() {
+				let bound = MinGasPrice::<T>::get() / T::MinGasPriceBoundDivisor::get() + U256::one();
+
+				let upper_limit = MinGasPrice::<T>::get().saturating_add(bound);
+				let lower_limit = MinGasPrice::<T>::get().saturating_sub(bound);
+
+				MinGasPrice::<T>::set(min(upper_limit, max(lower_limit, target)));
+			}
+
+			TargetMinGasPrice::<T>::kill();
+		}
+	}
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		#[pallet::weight(0)]
+		fn note_min_gas_price_target(
+			origin: OriginFor<T>,
+			target: U256,
+		) -> DispatchResultWithPostInfo {
+			ensure_none(origin)?;
+
+			TargetMinGasPrice::<T>::set(Some(target));
+			Self::deposit_event(Event::TargetMinGasPriceSet(target));
+			Ok(().into())
+		}
+	}
+}
+
+#[derive(Encode, Decode, RuntimeDebug)]
+pub enum InherentError { }
+
+impl IsFatalError for InherentError {
+	fn is_fatal_error(&self) -> bool {
+		match *self { }
+	}
+}
+
+pub const INHERENT_IDENTIFIER: InherentIdentifier = *b"dynfee0_";
+
+pub type InherentType = U256;
+
+#[cfg(feature = "std")]
+pub struct InherentDataProvider(pub InherentType);
+
+#[cfg(feature = "std")]
+impl ProvideInherentData for InherentDataProvider {
+	fn inherent_identifier(&self) -> &'static InherentIdentifier {
+		&INHERENT_IDENTIFIER
+	}
+
+	fn provide_inherent_data(
+		&self,
+		inherent_data: &mut InherentData
+	) -> Result<(), sp_inherents::Error> {
+		inherent_data.put_data(INHERENT_IDENTIFIER, &self.0)
+	}
+
+	fn error_to_string(&self, _: &[u8]) -> Option<String> {
+		None
+	}
+}
+
+impl<T: Config> ProvideInherent for Pallet<T> {
+	type Call = Call<T>;
+	type Error = InherentError;
+	const INHERENT_IDENTIFIER: InherentIdentifier = INHERENT_IDENTIFIER;
+
+	fn create_inherent(data: &InherentData) -> Option<Self::Call> {
+		let target = data.get_data::<InherentType>(&INHERENT_IDENTIFIER).ok()??;
+
+		Some(Call::note_min_gas_price_target(target))
+	}
+
+	fn check_inherent(_call: &Self::Call, _data: &InherentData) -> result::Result<(), Self::Error> {
+		Ok(())
+	}
+}
